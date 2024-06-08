546603ce1ed8c38720ff25e16cda64f22368e837
Rafael Lacerda <rafaellacerda188@gmail.com>
2024-06-08 12:30:49 -0300

    Adiciona função JS

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Função para abrir popup de informações das estações
+// ao clicar no botão da página principal
 document.addEventListener('DOMContentLoaded', function() {
     var closeButton = document.querySelector('.popupInformacoesEstacoes .close');
     var openButton = document.getElementById('consulte');
@@ -14,6 +16,25 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
+// Função para abrir popup de informações das estações
+// ao clicar no ícone de situação das estações na página de rotas
+document.addEventListener('DOMContentLoaded', function() {
+    var closeButton = document.querySelector('.popupInformacoesEstacoes .close');
+    var openButton = document.getElementById('iconeEstacao');
+    var popup = document.querySelector('.popupInformacoesEstacoes');
+
+    // Abrir popup
+    openButton.addEventListener('click', function() {
+        popup.style.display = 'flex';
+    });
+
+    // Fechar popup
+    closeButton.addEventListener('click', function() {
+        popup.style.display = 'none';
+    });
+});
+
+// Função para abrir popup de envio de email
 document.addEventListener('DOMContentLoaded', function() {
     var closeButton = document.querySelector('.popupEntreEmContato .close');
     var openButton = document.getElementById('entreEmContato');
