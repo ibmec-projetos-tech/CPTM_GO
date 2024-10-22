6194df648a93a98eaf578a31e9db7664812a0bf0
Rafael188Lacerda <rafaellacerda188@gmail.com>
2024-10-21 22:41:45 -0300

    Botoes Origem/Destino/Consulte foram conectados a base de dados disponível

---
+++ data.js
@@ -0,0 +1,11 @@
+export const estacoes = [
+    { nome: "São Paulo - Morumbi", status: "Aberta", horario: "10:00", acessibilidade: true, permiteAnimais: false, tarifa: 5.0 },
+    { nome: "Butantã", status: "Fechada", horario: "10:00", acessibilidade: true, permiteAnimais: false, tarifa: 5.0 },
+    { nome: "Faria Lima", status: "Interditada", horario: "10:00", acessibilidade: true, permiteAnimais: false, tarifa: 5.0 },
+    { nome: "Taboão da Serra", status: "Aberta", horario: "10:00", acessibilidade: true, permiteAnimais: false, tarifa: 5.0 },
+    { nome: "Vila Sônia", status: "Aberta", horario: "10:00", acessibilidade: true, permiteAnimais: false, tarifa: 5.0 },
+    { nome: "Fradique Coutinho", status: "Aberta", horario: "10:00", acessibilidade: true, permiteAnimais: false, tarifa: 5.0 },
+    { nome: "Oscar Freire", status: "Aberta", horario: "10:00", acessibilidade: true, permiteAnimais: false, tarifa: 5.0 },
+    { nome: "Consolação", status: "Interditada", horario: "10:00", acessibilidade: true, permiteAnimais: false, tarifa: 5.0 },
+    { nome: "Anhangabaú", status: "Aberta", horario: "10:00", acessibilidade: true, permiteAnimais: false, tarifa: 5.0 }
+  ];
