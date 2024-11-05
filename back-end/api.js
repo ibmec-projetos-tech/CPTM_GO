43f1e250ff3f79bdf47f7abd268e3e7989dfe1d0
Novo Nome de Usuário <gabrielmarcolinoramosbh@gmail.com>
2024-11-05 16:34:19 -0300

    feature consultar estacoes

--- back-end/api.js
+++ back-end/api.js
@@ -27,3 +27,37 @@ fetch(url)
   .catch(error => {
     console.error('There was a problem with the fetch operation:', error);
   });
+
+import express from 'express';
+import { estacoes } from './data.js';
+import cors from 'cors';
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+
+// Rota para obter a lista de estações
+app.get('/api/estacoes', (req, res) => {
+    res.json(estacoes);
+});
+
+app.get('/api/rota', (req, res) => {
+    const { origem, destino } = req.query;
+
+    if (!origem || !destino) {
+        return res.status(400).json({ error: 'Origem e destino são necessários.' });
+    }
+    const rota = {
+        origem,
+        destino,
+        mensagem: `Rota de ${origem} para ${destino} calculada com sucesso!`
+    };
+    
+    res.json(rota);
+});
+
+// Iniciar o servidor
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
