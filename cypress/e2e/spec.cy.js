94a95badf14f8ba17ea2238ae8df5475c6916963
Novo Nome de Usuário <gabrielmarcolinoramosbh@gmail.com>
2024-11-12 09:30:01 -0300

    testes/cypress

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -17,3 +17,40 @@ describe('Teste de Consulta de Estações', () => {
     }).as('estacoesRequest');
   });
 });
+
+// Remover estação por id
+describe('Teste de Remoção de Estação', () => {
+  it('Deve remover uma estação', () => {
+    cy.intercept('DELETE', 'https://127.0.0.1:8000/api/v1/estacoes/:1', {
+      body: [
+        { nome: "Butantã", status: "Fechada", horario: "10:00", acessibilidade: true, permiteAnimais: false, tarifa: 5.0 },
+        { nome: "Faria Lima", status: "Interditada", horario: "10:00", acessibilidade: true, permiteAnimais: false, tarifa: 5.0 },
+        { nome: "Taboão da Serra", status: "Aberta", horario: "10:00", acessibilidade: true, permiteAnimais: false, tarifa: 5.0 },
+        { nome: "Vila Sônia", status: "Aberta", horario: "10:00", acessibilidade: true, permiteAnimais: false, tarifa: 5.0 },
+        { nome: "Fradique Coutinho", status: "Aberta", horario: "10:00", acessibilidade: true, permiteAnimais: false, tarifa: 5.0 },
+        { nome: "Oscar Freire", status: "Aberta", horario: "10:00", acessibilidade: true, permiteAnimais: false, tarifa: 5.0 },
+        { nome: "Consolação", status: "Interditada", horario: "10:00", acessibilidade: true, permiteAnimais: false, tarifa: 5.0 },
+        { nome: "Anhangabaú", status: "Aberta", horario: "10:00", acessibilidade: true, permiteAnimais: false, tarifa: 5.0 }
+      ]
+    }).as('estacoesRequest');
+  });
+});
+
+// Modifica uma estação por id
+describe('Teste de Modificação de Estação', () => {
+  it('Deve modificar uma estação', () => {
+    cy.intercept('PUT', 'https://127.0.0.1:8000/api/v1/estacoes/:1', {
+      body: [
+        { nome: "São Paulo - Morumbi", status: "Fechada", horario: "12:00", acessibilidade: true, permiteAnimais: true, tarifa: 6.0 },
+        { nome: "Butantã", status: "Fechada", horario: "10:00", acessibilidade: true, permiteAnimais: false, tarifa: 5.0 },
+        { nome: "Faria Lima", status: "Interditada", horario: "10:00", acessibilidade: true, permiteAnimais: false, tarifa: 5.0 },
+        { nome: "Taboão da Serra", status: "Aberta", horario: "10:00", acessibilidade: true, permiteAnimais: false, tarifa: 5.0 },
+        { nome: "Vila Sônia", status: "Aberta", horario: "10:00", acessibilidade: true, permiteAnimais: false, tarifa: 5.0 },
+        { nome: "Fradique Coutinho", status: "Aberta", horario: "10:00", acessibilidade: true, permiteAnimais: false, tarifa: 5.0 },
+        { nome: "Oscar Freire", status: "Aberta", horario: "10:00", acessibilidade: true, permiteAnimais: false, tarifa: 5.0 },
+        { nome: "Consolação", status: "Interditada", horario: "10:00", acessibilidade: true, permiteAnimais: false, tarifa: 5.0 },
+        { nome: "Anhangabaú", status: "Aberta", horario: "10:00", acessibilidade: true, permiteAnimais: false, tarifa: 5.0 }
+      ]
+    }).as('estacoesRequest');
+  });
+});
